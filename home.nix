3b09d0ca85b8916b34a9b90febc1d4813f3cb4d6
CKA <cka.txt@gmail.com>
2025-11-19 15:24:44 -0600

    Configure Ghostty, Oh My Posh

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   programs.home-manager.enable = true;
@@ -7,4 +7,19 @@
   home.homeDirectory = "/Users/cka";
 
   home.packages = with pkgs; [];
+
+  # Ghostty:
+  home.file.".config/ghostty/config".source = ./cfg/ghostty/config;
+
+  # Oh My Posh:
+  programs.oh-my-posh.enable = true;
+  programs.oh-my-posh.enableZshIntegration = true;
+
+  # Zsh:
+  programs.zsh.enable = true;
+  programs.zsh.enableCompletion = true;
+  programs.zsh.autosuggestion.enable = true;
+  programs.zsh.initContent = ''
+    eval "$(oh-my-posh init zsh)"
+  '';
 }
