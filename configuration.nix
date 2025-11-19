2503f169c1dcce6f866490765013022af6004abf
CKA <cka.txt@gmail.com>
2025-11-19 01:15:33 -0600

    Add Fonts; Configure dock; Add pkgs + homebrew brews + cask

--- configuration.nix
+++ configuration.nix
@@ -9,21 +9,63 @@
 
   # Packages:
   environment.systemPackages = with pkgs; [
+    fastfetch
     firefox
     git
+    neovim
+    neovide
     obsidian
+    spotify
     vim
+    vscode
   ];
 
   # Homebrew:
   homebrew = {
     enable = true;
-    brews = [ ];
-    casks = [ ];
+    brews = [
+      "mas"
+    ];
+    casks = [
+      "chromium"
+      "ghostty"
+    ];
     masApps = { };
     onActivation.cleanup = "zap";
   };
 
+  # Fonts:
+  fonts.packages = with pkgs; [
+    nerd-fonts.blex-mono
+    nerd-fonts.fira-code
+    nerd-fonts.hack
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.sauce-code-pro
+    nerd-fonts.symbols-only
+    nerd-fonts.terminess-ttf
+    nerd-fonts.ubuntu
+    nerd-fonts.ubuntu-mono
+    nerd-fonts.ubuntu-sans
+  ];
+
+  # Configure dock:
+  system.defaults.dock = {
+    autohide = true;
+    autohide-delay = 0.0;
+    autohide-time-modifier = 0.2;
+    expose-animation-duration = 0.2;
+    launchanim = false;
+    persistent-apps = [
+      "/Applications/Ghostty.app"
+      "${pkgs.firefox}/Applications/Firefox.app"
+      "${pkgs.vscode}/Applications/Visual Studio Code.app"
+      "${pkgs.obsidian}/Applications/Obsidian.app"
+      "${pkgs.spotify}/Applications/Spotify.app"
+    ];
+    show-recents = false;
+    show-process-indicators = true;
+    tilesize = 48;
+  };
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
